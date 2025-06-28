82eabdce97425b575e779296a966a2ed9ee847d4
dmi3midd <dmi3midd@gmail.com>
2025-06-28 16:24:34 +0300

    Added hide/show password

--- src/components/ForModalNewPw/InputBlockForNewPw.jsx
+++ src/components/ForModalNewPw/InputBlockForNewPw.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { Eye, EyeOff } from 'lucide-react';
 
 export default function InputBlockForNewPw({state, setState, name, type, settings}) {
+    const [visiblePw, setVisisblePw] = useState(false);
+    
+    if (type === "password") {
+        return (        
+            <div>
+                <label style={{ color: settings.modalNewPassword.text }} className="block text-[20px]">{name}</label>
+                <div className="flex justify-between items-center w-[260px]">
+                    <input
+                        style={{ borderColor: settings.modalNewPassword.inputborder, color: settings.modalNewPassword.text }}
+                        className="block w-[80%] h-[30px] p-[5px] rounded-[8px] border-2 text-[15px] duration-200 focus:scale-107"
+                        type={!visiblePw ? "password" : "text"}
+                        placeholder={name}
+                        value={state}
+                        onChange={ev => setState(ev.target.value)}
+                    />
+                    {visiblePw 
+                        ? <Eye color={`${settings.modalNewPassword.text}`} size={25} onClick={() => setVisisblePw(false)}/> 
+                        : <EyeOff color={`${settings.modalNewPassword.text}`} size={25} onClick={() => setVisisblePw(true)}/>
+                    }
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div>
             <label style={{ color: settings.modalNewPassword.text }} className="block text-[20px]">{name}</label>
-            <input 
-                style={{ borderColor: settings.modalNewPassword.inputborder, color: settings.modalNewPassword.text }} 
+            <input
+                style={{ borderColor: settings.modalNewPassword.inputborder, color: settings.modalNewPassword.text }}
                 className="block w-[80%] h-[30px] p-[5px] rounded-[8px] border-2 text-[15px] duration-200 focus:scale-107"
                 type={type}
                 placeholder={name}
--- src/components/ForModalViewPw/EditedEntry/ItemForEditedEntry.jsx
+++ src/components/ForModalViewPw/EditedEntry/ItemForEditedEntry.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Eye, EyeOff } from 'lucide-react';
 
 export default function ItemForEditedEntry({state, setState, name, type, settings}) {
+    const [visiblePw, setVisisblePw] = useState(false);
+    if (type === "password") {
+        return (
+            <div className="flex justify-between items-center w-[350px] pb-[5px]">
+                <label style={{ color: settings.modalViewPassword.text }}>{name}</label>
+                <div className="flex items-center justify-end w-[300px]">
+                    <input
+                        style={{ borderColor: settings.modalViewPassword.inputborder, color: settings.modalViewPassword.text }}
+                        className="block w-[220px] h-[30px] p-[5px] rounded-[8px] border-2 text-[15px] duration-200 focus:scale-107"
+                        type={!visiblePw ? "password" : "text"}
+                        value={state}
+                        onChange={ev => setState(ev.target.value)}
+                    />
+                    {visiblePw
+                        ? <Eye color={`${settings.modalNewPassword.text}`} size={25} onClick={() => setVisisblePw(false)} />
+                        : <EyeOff color={`${settings.modalNewPassword.text}`} size={25} onClick={() => setVisisblePw(true)} />
+                    }
+                </div>
+            </div>
+        )
+    }
     return (
         <div className="flex justify-between items-center w-[350px] pb-[5px]">
             <label style={{ color: settings.modalViewPassword.text }}>{name}</label>
-            <input 
-                style={{ borderColor: settings.modalViewPassword.inputborder, color: settings.modalViewPassword.text }} 
+            <input
+                style={{ borderColor: settings.modalViewPassword.inputborder, color: settings.modalViewPassword.text }}
                 className="block w-[70%] h-[30px] p-[5px] rounded-[8px] border-2 text-[15px] duration-200 focus:scale-107"
                 type={type}
                 value={state}
